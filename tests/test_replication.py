5890607902596013dd37ce233a79aa6a8be4967a
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2023-09-30 22:55:29 +0100

    better system test + add more logs

--- tests/test_replication.py
+++ tests/test_replication.py
@@ -1,4 +1,5 @@
 import os
+
 from http_utility import append_message, get_messages
 
 PRIMARY_URL = os.getenv("PRIMARY_URL")
@@ -8,16 +9,18 @@ SECONDARY2_URL = os.getenv("SECONDARY2_URL")
 
 def test_basic_replication() -> None:
     # GIVEN
-    message = "test"
+    messages = ["msg 1", "msg 2", "msg 3", "msg 4", "msg 5"]
+
     # WHEN
-    is_added = append_message(PRIMARY_URL, message)
+    for message in messages:
+        is_added = append_message(PRIMARY_URL, message)
+        assert is_added, "Failed to append message: " + message
 
     messages_primary = get_messages(PRIMARY_URL)
     messages_secondary1 = get_messages(SECONDARY1_URL)
     messages_secondary2 = get_messages(SECONDARY2_URL)
-    # THEN
-    assert is_added, "Failed to append a message"
 
-    assert message in messages_primary, "No message in PRIMARY storage"
-    assert message in messages_secondary1, "No message in SECONDARY_1 storage"
-    assert message in messages_secondary2, "No message in SECONDARY_2 storage"
+    # THEN
+    assert messages == messages_primary, "Incorrect messages in PRIMARY storage"
+    assert messages == messages_secondary1, "Incorrect messages in SECONDARY_1 storage"
+    assert messages == messages_secondary2, "Incorrect messages in SECONDARY_2 storage"
