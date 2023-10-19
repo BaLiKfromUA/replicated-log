3be731ccca3a25b5a0c30b74c64adbe3f0980c8a
Valentyn Yukhymenko <valentin.yukhymenko@gmail.com>
2023-10-19 23:43:47 +0100

    better

--- tests/test_replication.py
+++ tests/test_replication.py
@@ -4,11 +4,12 @@ from http_utility import append_message, get_messages, block_replication
 def test_basic_replication(primary_url, secondary1_url, secondary2_url) -> None:
     """simple test to ensure that whole system works as expected from 1 iteration"""
     # GIVEN
+    w = 3
     messages = ["msg 1", "msg 2", "msg 3", "msg 4", "msg 5"]
 
     # WHEN
     for message in messages:
-        is_added = append_message(primary_url, message, 3)
+        is_added = append_message(primary_url, message, w)
         assert is_added, "Failed to append message: " + message
 
     messages_primary = get_messages(primary_url)
@@ -31,6 +32,7 @@ def test_inconsistency_and_eventual_consistency_during_replication_with_w_2(prim
     After delay on the secondary node, messages lists must be the same on all nodes
     """
     # GIVEN
+    w = 2
     messages = ["msg 1", "msg 2", "msg 3", "msg 4", "msg 5"]
 
     # WHEN
@@ -38,7 +40,7 @@ def test_inconsistency_and_eventual_consistency_during_replication_with_w_2(prim
     assert is_blocked
 
     for message in messages:
-        is_added = append_message(primary_url, message, 2)
+        is_added = append_message(primary_url, message, w)
         assert is_added, "Failed to append message: " + message
 
     messages_primary = get_messages(primary_url)
@@ -70,6 +72,7 @@ def test_inconsistency_and_eventual_consistency_during_replication_with_w_1(prim
     After delay on the secondary nodes, messages lists must be the same on all nodes
     """
     # GIVEN
+    w = 1
     messages = ["msg 1", "msg 2", "msg 3", "msg 4", "msg 5"]
 
     # WHEN
@@ -79,7 +82,7 @@ def test_inconsistency_and_eventual_consistency_during_replication_with_w_1(prim
     assert is_blocked
 
     for message in messages:
-        is_added = append_message(primary_url, message, 1)
+        is_added = append_message(primary_url, message, w)
         assert is_added, "Failed to append message: " + message
 
     messages_primary = get_messages(primary_url)
